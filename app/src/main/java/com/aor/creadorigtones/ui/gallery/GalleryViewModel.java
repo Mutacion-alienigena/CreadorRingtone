7c8be96d834eaaf6c44ff60c51025b056f7b4036
matiasortiz8167 <matias.ortiz1@davinci.edu.ar>
2020-06-09 00:27:11 -0300

    prueba

--- app/src/main/java/com/aor/creadorigtones/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/aor/creadorigtones/ui/gallery/GalleryViewModel.java
@@ -10,7 +10,7 @@ public class GalleryViewModel extends ViewModel {
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("Obtener Mp3 de un video");
     }
 
     public LiveData<String> getText() {
